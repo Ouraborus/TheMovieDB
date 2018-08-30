591897a0dbbdb3b7349248c07644dba7363c72d3
Esteban Cardona Alvarez <esteban.alvarez@co-it00999.globant.com>
2018-08-30 10:09:07 -0500

    Updated storyboard, added new elements to the Detail View, need to work with the landscape view

--- TheMovieDB/TheMovieDB/Controller/MovieDetailViewController.swift
+++ TheMovieDB/TheMovieDB/Controller/MovieDetailViewController.swift
@@ -17,15 +17,27 @@ import AlamofireImage
 //}
 
 class MovieDetailViewController: UIViewController {
+    @IBOutlet weak var moviePoster: UIImageView!
     @IBOutlet weak var movieImage: UIImageView!
     @IBOutlet weak var movieTitle: UILabel!
-    @IBOutlet weak var movieOverview: UILabel!
+    @IBOutlet weak var movieOverview: UITextView!
+    @IBOutlet weak var voteCount: UILabel!
+    @IBOutlet weak var voteAverage: UILabel!
+    @IBOutlet weak var releaseDate: UILabel!
+    @IBOutlet weak var popularity: UILabel!
+    
     var movie: Movie?
     override func viewDidLoad() {
         super.viewDidLoad()
+        moviePoster.af_setImage(withURL:(movie?.backdrop_path)!)
         movieImage.af_setImage(withURL: (movie?.poster_path)!)
         movieTitle.text = movie?.title
         movieOverview.text = movie?.overview
+        voteCount.text = "Vote Count: \(movie?.vote_count ?? 0)"
+        voteAverage.text = "Vote Average: \(movie?.vote_average ?? 0)"
+        releaseDate.text = "Release Date: \(movie?.release_date ?? "Not Avaiable")"
+        popularity.text = "Popularity: \(movie?.popularity ?? 0)"
+        
     }
 }
 //extension MovieDetailViewController: MovieDetail {
